45be2e32a9b08f9f94034f8918877439e56e2dc9
artytrik <tema-luch@mail.ru>
2020-02-13 10:55:55 +0300

    Добавляет слайдер

--- source/contact.php
+++ source/contact.php
@@ -1,7 +1,7 @@
 <?php
 if (isset ($_POST['form-name'])) {
   $to = "tema-luch@mail.ru";
-  /*$from = "";*/
+  $from = "tema-luch@mail.ru";
   $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
   $message = "Имя пользователя: ".$_POST['form-name']."\nТелефон пользователя ".$_POST['form-telephone']."\nСообщение: ".$_POST['form-message']."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
 
---
+++ source/js/slider.js
@@ -0,0 +1,32 @@
+$('.carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
