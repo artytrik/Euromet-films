8f38b3a213f0d3bd9878bcd5b91891be32f62dc5
artytrik <tema-luch@mail.ru>
2020-02-26 16:41:09 +0300

    Добавляет слайдер

--- build/js/swiper.js
+++ build/js/swiper.js
@@ -1,31 +1,11 @@
 "use strict";
 
-var _swiper = _interopRequireDefault(require("swiper"));
-
-require("../../node_modules/swiper/css/swiper.css");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 var partnersSwiper;
-var principlesSwiper = new _swiper["default"](".principles-slider", {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function renderBullet(index, className) {
-      return '<span class="' + className + '">' + 0 + (index + 1) + '</span>';
-    }
-  },
-  loop: true
-});
 var breakpoint = window.matchMedia('(max-width:1149px)');
 
 var enableSwiper = function enableSwiper() {
-  partnersSwiper = new _swiper["default"](".partners__swiper", {
-    slidesPerView: 2,
+  partnersSwiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
     autoplay: {
