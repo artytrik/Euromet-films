f69f208fa588c54ffe70786d19a9935e0d74f81e
artytrik <tema-luch@mail.ru>
2020-03-12 14:53:00 +0300

    добавляет форму на странице о нас

--- source/send.php
+++ source/send.php
@@ -12,17 +12,14 @@
     $data['success'] = false;
     $data['errors'] = $errors;
   } else {
-    $to = "mail@euromet.m-n.agency";; // Здесь нужно написать e-mail, куда будут приходить письма
-    $from = "mail@euromet.m-n.agency"; // this is the sender's Email address
+    $to = "mail@euromet.m-n.agency";;
+    $from = "mail@euromet.m-n.agency";
 
     $first_name = $_POST['call-name'];
-    $subject = "Форма отправки сообщений с сайта M&N Agency";
-    //$subject2 = "Copy of your form submission";
+    $subject = "Форма отправки сообщений с сайта Euromet Films";
     $message = $first_name . " оставил телефон:" . "\n\n" . $_POST['call-tel'];
-    //$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    //$headers2 = "From:" . $to;
 
     mail($to,$subject,$message,$headers);
 
